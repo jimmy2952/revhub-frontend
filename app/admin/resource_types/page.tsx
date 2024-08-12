680fa5baefe0f08eb7733d6eb481920e00968a74
Jimmy Chen <jimmy2952@gmail.com>
2024-08-12 08:48:11 +0800

    feat: adding alert button

--- app/admin/resource_types/page.tsx
+++ app/admin/resource_types/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import Link from "next/link"
 import { format } from "date-fns"
 import { Plus } from "lucide-react"
@@ -12,9 +14,15 @@ import {
 } from "@/components/ui/table"
 import PageHeader from "@/components/ui/PageHeader"
 import { Button } from "@/components/ui/button"
+import ConfirmDialog from "@/components/ui/ConfirmDialog"
 import { resourceTypes } from "@/app/lib/placeholder-data"
 
 export default function ResourceTypesIndex() {
+
+  const onResourceTypeDelete = (id: number) => {
+    console.log(`Resource type id: ${id} will be deleted`)
+  }
+
   return (
     <div className="mx-auto flex h-screen max-w-screen-xl flex-col items-center justify-center">
       <PageHeader className="flex gap-x-4">
@@ -49,7 +57,13 @@ export default function ResourceTypesIndex() {
               </TableCell>
               <TableCell className="flex justify-center gap-x-2">
                 <Button>Edit</Button>
-                <Button variant="destructive">Delete</Button>
+                <ConfirmDialog
+                  title="Are you sure?"
+                  description={`Resource type id: ${resourceType.id} will be deleted`}
+                  onConfirm={() => { onResourceTypeDelete(resourceType.id) }}
+                >
+                  <Button variant="destructive">Delete</Button>
+                </ConfirmDialog>
               </TableCell>
             </TableRow>
           ))}
